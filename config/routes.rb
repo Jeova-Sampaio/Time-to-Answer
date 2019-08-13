ba639ed9c27ae597ce7d2b6a79750147047d366f
Jeova-Sampaio <jeova_samp@hotmail.com>
2019-08-13 15:46:44 -0300

    Criando as views (index e delete) para assuntos_areas

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins #Administradores
+    resources :admins   #Administradores
+    resources :subjects #Assuntos/areas
   end
 
   devise_for :users
