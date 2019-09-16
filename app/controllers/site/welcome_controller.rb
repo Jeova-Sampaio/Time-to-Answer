f65d537a927250700c6764773e5eb428ceb01cb7
Jeova-Sampaio <jeova_samp@hotmail.com>
2019-09-16 15:20:26 -0300

    Usando metodos de classe no model

--- app/controllers/site/welcome_controller.rb
+++ app/controllers/site/welcome_controller.rb
@@ -1,6 +1,6 @@
 class Site::WelcomeController < SiteController
   def index
     # @questions = Question.all. Da forma que está resolve o problema do N+1! 
-    @questions = Question.includes(:answers).page(params[:page]) 
+    @questions = Question.last_questions(params[:page]) 
   end
 end
