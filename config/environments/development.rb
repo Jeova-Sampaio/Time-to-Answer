e2ab03be4d2e1a5dced4b0c7a0a64612253967b7
Jeova <jeova_samp@hotmail.com>
2019-12-04 10:30:03 -0300

    Mailtrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,18 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+  # Mailtrap config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '6e138ff2d42973',
+    :password => '407fd59ded62b8',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
